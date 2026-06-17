697bc04e2152a5cb8670e4d5b4f492ee6529ee7c
TravaChipzzxk <allysonrodolfozk@gmail.com>
2026-06-16 21:35:20 -0300

    Update test_arquivo-ox.c

--- test_arquivo-ox.c
+++ test_arquivo-ox.c
@@ -1,15 +1,14 @@
 #include <assert.h>
 
-
-int test_valide(int valide1, int valide2, int valide3, int valide4);
+int test_valide(char c);
 
 int main()
 {
-    assert(valide1('X') == 1);
-    assert(valide2('O') == 1);
+    assert(test_valide('X') == 1);
+    assert(test_valide('O') == 1);
 
-    assert(valide3('A') == 0);
-    assert(valide4('x') == 0);
+    assert(test_valide('A') == 0);
+    assert(test_valide('x') == 0);
 
     return 0;
 }
